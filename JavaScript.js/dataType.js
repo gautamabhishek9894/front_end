b488bd36ede6ba111c5d31671650f7db2c1cad93
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-01-19 13:11:23 +0530

    js

---
+++ JavaScript.js/dataType.js
@@ -0,0 +1,11 @@
+var name='abhishek';
+var surname='gautam';
+var maried='false';
+var age='22';
+var ans='${name} , ${surname}, ${maried},${age}';
+for(let i=0;i<3; i++){
+    console.log(i);
+}
+let val=3;
+val='gautam';
+console.log(val);
