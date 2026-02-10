34baabde605c2c182c175b19f13ff30181e50801
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-02-10 13:06:33 +0530

    my code

--- JavaScript/SwitchCase.js
+++ JavaScript/SwitchCase.js
@@ -1,28 +1,51 @@
-const value=undefined;
-switch(null){
-    case true:
-        console.log('yes');
-        break;
+// const value=undefined;
+// switch(null){
+//     case true:
+//         console.log('yes');
+//         break;
 
-        case false:
-            console.log('no');
-            break;
+//         case false:
+//             console.log('no');
+//             break;
 
-            default:
-                console.log('please select boolean value true / false');       
+//             default:
+//                 console.log('please select boolean value true / false');       
+// }
+
+// Function
+let num1=+process.argv[2];
+let num2=+process.argv[3];
+let operators=process.argv[4];
+function calc(num1, num2, opts) {
+    switch (opts) {
+        case '*':
+            return num1 * num2;
+
+        case '-':
+            return num1 - num2;
+
+        case '/':
+            return num1 / num2;
+
+        case '+':
+            return num1 + num2;
+
+        default:
+            return null;
+    }
+}
+const result=calc(num1,num2, operators);
+if(result===null){
+    console.log('please select correct operators!');
+}       
+else{
+    console.log(Math.abs(result));
 }
 
-//  Function
-
-function sumofvalue(a, b, c) {
-    console.log(a + b + c);
-    return a + b + c;    
-}   
-console.log(sumofvalue(10, 6, 12));
-const arrowFunc = (a, b, c) => {
-    return a + b + c;
+const addFunc=(A,B)=>{
+    return A+B;
 };
-console.log(arrowFunc(1, 2, 3));
+console.log(addFunc(num1,num2));
 
 
 
---
+++ JavaScript/object.js
@@ -0,0 +1,26 @@
+let name='abhishek';
+console.log(name);
+
+let obj={
+    name:'gautam'
+};
+
+// # read
+console.log(obj.name);
+
+// ? Create
+obj.age=22;
+console.log(obj);
+
+// update
+obj.age=23;
+console.log(obj);
+
+
+// // delete
+delete obj.age;
+console.log(obj);
+
+                               // CRUD Operation
+
+
