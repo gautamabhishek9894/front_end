5e7525fc3f0d872cbab1d85c99c8c46a9ccf4d9d
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-02-11 13:09:12 +0530

    code of the day

---
+++ JavaScript/array_looping.js
@@ -0,0 +1,60 @@
+let array=[4,3, true, false,{name:'abhishek'},['shivam', 'suraj'],'renee'];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+
+// for("intialization","condition","increment++"/"decrement--"){
+//     // code of output
+// }
+
+for(i=0;i<array.length;i++){
+    console.log(array[i]);
+}
+
+//   print number from 1 to 10  using the loop.
+// let arr1=[1,2,3,4,5,6,7,8,9,10]
+// for(i=0;i<arr1.length;i++){
+//     console.log(arr1[i]);
+// }
+
+// print all even and odd number 1 and 50.
+// for ( let i=1;i<=50;i++){
+//     if(i%2===0){
+//         console.log(`this is even number :- ${i}`)
+//     }
+//     else{
+//         console.log(`this is odd number :- ${i}`)
+        
+//     }
+// }
+
+// print all multiplication number   table of the 5.
+// for(let i=1;i<=10;i++){
+//     console.log(`5x${i}=${5*i}`);
+// }
+
+// print  number from  100 downn  to 1(reverse order).
+// for(i=100; i>=1;i--){
+//     console.log(i);
+// }
+
+// print the sum of the from the 1 to 100.
+// for(let i=1; i<100;i++){
+//     console.log(i++);
+    
+// }
+// print the square of the number first  10natural number.
+// for(let i=1;i<=10;i++){
+
+//     console.log(`${i**2}`);
+// }
+
+// print the cube of the first 10  natural number .
+for(let i=1;i<=10;i++){
+    console.log(`${i**3}`);
+}
