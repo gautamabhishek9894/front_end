7b36b6383ce7473f70b4a8b31f3a94aa5c6bdd70
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-02-18 11:31:23 +0530

    my code

--- JavaScript/SwitchCase.js
+++ JavaScript/SwitchCase.js
@@ -47,6 +47,258 @@ const addFunc=(A,B)=>{
 };
 console.log(addFunc(num1,num2));
 
+// Questions ///////////////////////////////////
+
+// // const value=undefined;
+// // switch(null){
+// //     case true:
+// //         console.log('yes');
+// //         break;
+
+// //         case false:
+// //             console.log('no');
+// //             break;
+
+// //             default:
+// //                 console.log('please select boolean value true / false');       
+// // }
+
+// // Function
+// // let num1=+process.argv[2];
+// // let num2=+process.argv[3];
+// // let operators=process.argv[4];
+// // function calc(num1, num2, opts) {
+// //     switch (opts) {
+// //         case '*':
+// //             return num1 * num2;
+
+// //         case '-':
+// //             return num1 - num2;
+
+// //         case '/':
+// //             return num1 / num2;
+
+// //         case '+':
+// //             return num1 + num2;
+
+// //         default:
+// //             return null;
+// //     }
+// // }
+// // const result=calc(num1,num2, operators);
+// // if(result===null){
+// //     console.log('please select correct operators!');
+// // }       
+// // else{
+// //     console.log(Math.abs(result));
+// // }
+
+// // const addFunc=(A,B)=>{
+// //     return A+B;
+// // };
+// // console.log(addFunc(num1,num2));
+
+
+// // Switch  case
+
+// // 1. Take a number (1–7). Print the day of the week.
+// // switch (day) {
+// //   case 1:
+// //     console.log("Sunday");
+// //     break;
+// //   case 2:
+// //     console.log("Monday");
+// //     break;
+// //   case 3:
+// //     console.log("Tuesday");
+// //     break;
+// //   case 4:
+// //     console.log("Wednesday");
+// //     break;
+// //   case 5:
+// //     console.log("Thursday");
+// //     break;
+// //   case 6:
+// //     console.log("Friday");
+// //     break;
+// //   case 7:
+// //     console.log("Saturday");
+// //     break;
+// //   default:
+// //     console.log("Invalid day number");
+// // }
+
+// // 2. Take a month number (1–12). Print the month name.
+// let month=1;
+// switch(month){
+//     case 1:
+//         console.log('january');
+//         break;
+//         case 2:
+//         console.log('fabruary');
+//         break;
+//         case 3:
+//         console.log('march');
+//         break;
+//         case 4:
+//         console.log('april');
+//         break;
+//         case 5:
+//         console.log('may');
+//         break;
+//         case 6:
+//         console.log('june');
+//         break;
+//         case 7:
+//         console.log('july');
+//          break;
+//          case 8:
+//          console.log('august');
+//          break;
+//          case 9:
+//          console.log('september');
+//         break;
+//         case 10:
+//         console.log('october');
+//         break;
+//         case 11:
+//          console.log('november');
+//         break;
+//         case 12:
+//          console.log('december');
+//         break;
+//         default:
+//         console.log('invalid month');
+  
+// }
+
+// // 3. Input a number (1–4). Perform Addition, Subtraction, Multiplication, or Division.
+
+// let calc = 1;
+// let a = 10;
+// let b = 5;
+
+// switch (calc) {
+//   case 1:
+//     console.log(a + b);
+//     break;
+//   case 2:
+//     console.log(a - b);
+//     break;
+//   case 3:
+//     console.log(a * b);
+//     break;
+//   case 4:
+//     console.log(a / b);
+//     break;
+//   default:
+//     console.log("Invalid choice");
+// }
+
+// // 4. A user selects a drink: C → Coffee, T → Tea, J → Juice, W → Water.
+// let drink='T';
+// switch(drink){
+//     case 'T':
+//         console.log('cofee');
+//         break;
+//         case 'C':
+//         console.log('tea');
+//         break;
+//         case 'j':
+//        console.log('juice');
+//        break;
+//        case 'W':
+//        console.log('water');
+//        break;
+//        default:
+//        console.log("invalid drinks");      
+// }
+
+// // 5. Input a grade ('A', 'B', 'C', 'D', 'F'). Print a message like Excellent, Good, etc.
+// let grade="A";
+// switch(grade){
+//     case"A":
+//     console.log("Excellent");
+//     break;
+//     case"B":
+//     console.log("Good");
+//     break;
+//     case"C":
+//     console.log("Average");
+//     break;
+//     case"D":
+//     console.log("Poor");
+//     break;
+//     case"F":
+//     console.log("Fail");
+//     break;
+//     default:
+//     console.log("invalid grade");
+// }
+
+// // 6. Input the first letter of a traffic light ('R', 'Y', 'G'). Print Stop, Ready, or Go.
+
+// let signal = "R";
+
+// switch (signal) {
+//   case "R":
+//     console.log("Stop");
+//     break;
+//   case "Y":
+//     console.log("Ready");
+//     break;
+//   case "G":
+//     console.log("Go");
+//     break;
+//   default:
+//     console.log("Invalid signal");
+// }
+
+// // 7. Input a language code ('en', 'fr', 'es', 'de'). Display the language name.
+
+// let lang = "en";
+
+// switch (lang) {
+//   case "en":
+//     console.log("English");
+//     break;
+//   case "fr":
+//     console.log("French");
+//     break;
+//   case "es":
+//     console.log("Spanish");
+//     break;
+//   case "de":
+//     console.log("German");
+//     break;
+//   default:
+//     console.log("Language not supported");
+// }
+
+// // 7. Input a browser name (Chrome, Firefox, Edge, Safari). Show a launch message.
+
+// let browser = "Chrome";
+
+// switch (browser) {
+//   case "Chrome":
+//     console.log("Launching Chrome...");
+//     break;
+//   case "Firefox":
+//     console.log("Launching Firefox...");
+//     break;
+//   case "Edge":
+//     console.log("Launching Edge...");
+//     break;
+//   case "Safari":
+//     console.log("Launching Safari...");
+//     break;
+//   default:
+//     console.log("Browser not supported");
+// }
+
+
+
+
 
 
 
--- JavaScript/arrayMathod.js
+++ JavaScript/arrayMathod.js
@@ -15,12 +15,123 @@
 
 
 
-let new1=["abhishek", "shivam","suraj", "sneha"]
-let new2=new1.map((index, number, array)=>{
-    return new1.length;
-    console.log(new2);
+// let new1=["abhishek", "shivam","suraj", "sneha"]
+// let new2=new1.map((index, number, array)=>{
+//     return new1.length;
+//     console.log(new2);
     
-})
-  console.log(new2); 
+// })
+//   console.log(new2); 
 
 
+
+  let new3=[4,4,4,36]
+  let arr=new3.filter((el)=>el!=36)
+  console.log(arr);
+
+  // Questions ////////////////////////////
+
+
+  // push
+// Q1. Given an array [1,2,3], add 4 and 5 at the end.
+
+let arrA = [1,2,3];
+arr.push(4,5);
+console.log(arrA);
+
+// pop
+// Q2. Remove the last element from [10,20,30].
+let arr1 = [10,20,30];
+let removed = arr1.pop();
+console.log(arr1);
+console.log(removed);
+
+
+// shift
+// Q4. Remove the first element from [5,6,7].
+let arr3 = [5,6,7];
+arr3.shift();
+console.log(arr3);
+
+// unshift
+// Q5. Add 1 at the beginning of [2,3,4].
+let arr4 = [2,3,4];
+arr4.unshift(1);
+console.log(arr4);
+
+// splice
+// Q6. Create an array of 5 numbers. Remove the second element using splice().
+let number = [10, 20, 30, 40, 50];
+number.splice(1,1);
+console.log(number);
+
+// splice
+// Q7. Create an array of 4 fruit names. Add "Mango" at index 1 using splice().
+let fruits = ["Apple", "Banana", "Orange", "Grapes"];
+fruits.splice(1,0,"Mango");
+console.log(fruits);
+
+// slice
+// Q8. Create an array of 6 numbers. Copy the first 3 elements using slice().
+let arr5 = [1,2,3,4,5,6];
+let firstThree = arr5.slice(0,3);
+console.log(firstThree);
+console.log(arr5);
+
+// slice
+// Q9. Create an array of 5 city names. Copy the last 2 elements using slice().
+let cities = ["Delhi","Mumbai","Chennai","Kolkata","Pune"];
+let lastTwo = cities.slice(-2);
+console.log(lastTwo);
+console.log(cities);
+
+// map() //////////////////////////////////////////////////////////////////
+
+// Q1. Given a list of integers, produce a new list where each number is squared.
+let num = [1, 2, 3, 4];
+let squared = num.map(num => num * num);
+console.log(squared);
+
+
+// Q2. Given a list of names, return a list of their lengths.
+let names = ["John", "Anna", "Mike"];
+let lengths = names.map(name => name.length);
+console.log(lengths);
+
+// Q3. Convert a list of temperatures in Celsius to Fahrenheit.
+let celsius = [0, 10, 20, 30];
+let fahrenheit = celsius.map(temp => (temp * 9/5) + 32);
+console.log(fahrenheit);
+
+// Q4. Given a list of words, return a list where each word is capitalized.
+let word = ["apple", "banana", "mango"];
+let capitalized = words.map(word => word.toUpperCase());
+console.log(capitalized);
+
+// Q5. Given a list of numbers, return a list of their absolute values.
+let num1 = [-5, 3, -2, 8];
+let absoluteValues = num1.map(num => Math.abs(num));
+console.log(absoluteValues);
+
+//filter(). ///////////////////////////////////////////////////////////
+
+// Q1. From a list of integers, keep only the even numbers.
+let numbers = [1, 2, 3, 4, 5, 6];
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers);
+
+// Q2. From a list of numbers, keep only those greater than 50.
+let nums = [20, 55, 70, 10, 90];
+let greaterThan50 = nums.filter(num => num > 50);
+console.log(greaterThan50);
+
+// Q3. From a list of strings, keep only the strings longer than 5 characters.
+let words = ["apple", "banana", "kiwi", "orange"];
+let longWords = words.filter(word => word.length > 5);
+console.log(longWords);
+
+// Q4. From a list of numbers, keep only the positive numbers.
+let values = [-10, 5, -3, 8, 0];
+let positiveNumbers = values.filter(num => num > 0);
+console.log(positiveNumbers);
+  
