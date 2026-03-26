0ca4e57eb808b7ff328e7cbc0434aeb0d1f97fba
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-03-26 13:21:23 +0530

    my code

--- Counter_Task_JS/index.js
+++ Counter_Task_JS/index.js
@@ -1,19 +1,28 @@
 let count=0;
+const dec = document.getElementById('dec')
+dec.disabled=true;
 const counterUI=document.getElementById('Counter_UI');
 const incFunc=()=>{
     count++;
     counterUI.innerHTML='';
     counterUI.append(`Counter: ${count}`);
+    if(count>0){
+        dec.disabled=false;
+    }
 };
-const countUI=document.getElementById('Counter_UI');
 const resFunc=()=>{
     count=0;
-    countUI.innerHTML='';
-    countUI.append(`Counter: ${count}`);
+    counterUI.innerHTML='';
+    counterUI.append(`Counter: ${count}`);
+    if(count<=0){
+        dec.disabled=true;
+    }
 };
-const count1=document.getElementById('Counter_UI');
 const decFunc=()=>{
     count--;
-    count1.innerHTML='';
-    count1.append(`Counter: ${count}`);
+    counterUI.innerHTML='';
+    counterUI.append(`Counter: ${count}`);
+    if(count>=0){
+        dec.disabled=true;
+    }
 };
