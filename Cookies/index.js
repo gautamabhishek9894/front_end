d3924f2ebd6edee740ac061bde65b1ced4c9a48f
gautamabhishek9894 <gautamabhishek9894@gmail.com>
2026-05-15 12:59:57 +0530

    code

---
+++ Cookies/index.js
@@ -0,0 +1,13 @@
+let data=new Date();
+console.log(date:before,date);
+
+data.setTime(date.t)
+
+
+utc==>universal time cordinated
+gmt==>greenwich mean time
+ist==>india standard time
+
+document.cookie='netfilxToken=abhishek; expires: 
+
+
